71f8b699d3a6561c0e57a0313ede7461acbe05f3
vicentesantos <vicente.goncalves@fatec.sp.gov.br>
2022-04-13 20:54:09 -0300

    Configuração de cliente

---
+++ sig1/src/main/java/com/fatec/sig1/ports/ClienteRepository.java
@@ -0,0 +1,14 @@
+package com.fatec.sig1.ports;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.fatec.sig1.model.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+	Optional<Cliente> findByCpf(String cpf);
+
+	List<Cliente> findAllByNomeIgnoreCaseContaining(String nome);
+}
---
+++ sig1/src/main/java/com/fatec/sig1/ports/MantemCliente.java
@@ -0,0 +1,19 @@
+package com.fatec.sig1.ports;
+
+import java.util.List;
+import java.util.Optional;
+import com.fatec.sig1.model.Cliente;
+
+public interface MantemCliente {
+	List<Cliente> consultaTodos();
+
+	Optional<Cliente> consultaPorCpf(String cpf);
+
+	Optional<Cliente> consultaPorId(Long id);
+
+	Optional<Cliente> save(Cliente cliente);
+
+	void delete(Long id);
+
+	Optional<Cliente> altera(Cliente cliente);
+}

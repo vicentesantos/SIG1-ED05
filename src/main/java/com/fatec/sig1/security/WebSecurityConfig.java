b9ad31e9f3a4f73b0233f575b636f308d2809d3e
vicentesantos <vicente.goncalves@fatec.sp.gov.br>
2022-04-14 14:44:55 -0300

    Página de produto com erro 404

--- src/main/java/com/fatec/sig1/security/WebSecurityConfig.java
+++ src/main/java/com/fatec/sig1/security/WebSecurityConfig.java
@@ -12,8 +12,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/sig/cliente").hasAnyRole("ADMIN", "BIB") //
-				.antMatchers("/sig/cliente/{id}").hasRole("ADMIN") // somente login jose pode excluir
+		http.authorizeRequests().antMatchers("/sig/cliente", "/sig/produto").hasAnyRole("ADMIN", "BIB") //
+				.antMatchers("/sig/cliente/{id}", "/sig/produto").hasRole("ADMIN") // somente login jose pode excluir
 				.anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and().logout()
 				.logoutUrl("/login?logout").permitAll();
 	}
